34be3e786896549c8e32326668bfe36fe46fe3aa
ykan <nameyukan@gmail.com>
2021-07-23 11:58:27 +0800

    chore: eslint ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ignorePatterns: ['**/dist/*.js'],
+  ignorePatterns: ['**/dist/*.js', '**/_site/*'],
   extends: ['alloy', 'alloy/react', 'alloy/typescript', 'prettier'],
   env: {
     browser: true,
