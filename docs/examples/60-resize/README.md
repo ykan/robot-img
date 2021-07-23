aff0f08f1dcac085130c70f9e6d4a2e05deb6a94
ykan <nameyukan@gmail.com>
2021-07-23 16:15:27 +0800

    docs: 文档更新

---
+++ docs/examples/60-resize/README.md
@@ -0,0 +1,15 @@
+### 自适应处理图片
+
+图片组件会根据图片的面积自动适应，默认的判断方法：
+
+```ts
+function defaultShouldUpdate(newRect: DOMRect, oldRect: DOMRect) {
+  const newArea = newRect.width * newRect.height
+  const oldArea = oldRect.width * oldRect.height
+
+  // 当面积变大 20% 时，才更新图片
+  return newArea > oldArea * 1.2
+}
+```
+
+也可以通过给组件传 `shouldUpdate` 来定义图片是否需要更新
--- docs/examples/60-resize/index.tsx
+++ docs/examples/60-resize/index.tsx
@@ -2,20 +2,74 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import styled from '@emotion/styled'
-import { Img, imgPool } from '@robot-img/react-img'
-import { checkWebpSupported, createSrcTplOfAliOss } from '@robot-img/utils'
+import {
+  checkWebpSupported,
+  createSrcTplOfAliOss,
+  imgPool,
+  ImgProps,
+  useImg,
+} from '@robot-img/react-img'
 
 const Container = styled.div`
-  max-width: 960px;
-  margin: 0 auto;
-  padding: 10px;
-
   .robot-img {
-    width: 40vw;
-    height: 30vw;
     background-size: cover;
+    background-position: center;
+    transition: background-image 0.3s;
+    margin-bottom: 20px;
+  }
+  button {
+    margin-right: 10px;
+    user-select: none;
   }
 `
+
+const StyledImg = styled.div<{ $src: string }>`
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: background-image 0.3s;
+  ${(props) => props.$src && `background-image: url(${props.$src});`}
+`
+
+// 使用 useImg 自定义组件
+const Img = React.forwardRef<HTMLDivElement, ImgProps<HTMLDivElement>>((props, ref) => {
+  const { domProps, state, handleRef } = useImg(props, ref)
+  return (
+    <div>
+      <StyledImg {...domProps} $src={state.src} ref={handleRef} />
+      <p>当前的 src: ${state.src}</p>
+    </div>
+  )
+})
+
+function App() {
+  const [ratio, setRatio] = React.useState(1)
+  const handleAdd = React.useCallback(() => {
+    setRatio((oldRatio) => Math.min(3, oldRatio * 1.05))
+  }, [])
+  const handleCut = React.useCallback(() => {
+    setRatio((oldRatio) => oldRatio * 0.95)
+  }, [])
+  const imgStyle = React.useMemo(
+    () => ({
+      width: 100 * ratio,
+      height: 60 * ratio,
+    }),
+    [ratio]
+  )
+
+  return (
+    <Container>
+      <Img
+        style={imgStyle}
+        lazy="resize"
+        src="//image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg"
+      />
+      <button onClick={handleAdd}>宽高变大10%</button>
+      <button onClick={handleCut}>宽高变小10%</button>
+    </Container>
+  )
+}
 
 async function main() {
   const webp = await checkWebpSupported()
@@ -27,14 +81,7 @@ async function main() {
       className: 'robot-img',
     },
   })
-  ReactDOM.render(
-    <React.StrictMode>
-      <Container>
-        <Img.Div lazy="resize" src="//image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg" />
-      </Container>
-    </React.StrictMode>,
-    document.getElementById('60-resize')
-  )
+  ReactDOM.render(<App />, document.getElementById('60-resize'))
 }
 
 main()
